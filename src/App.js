229dc30760c438b8b264ffd790e4a95f9ae5f367
gachez <briangach98@gmail.com>
2019-12-25 01:00:18 +0300

    homepage structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import cart from './images/shopping-cart.png'
+import search from './images/magnifying-glass.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Homepage">
+
+      {/* menu */}
+      <nav className="menu">
+        <p id="logo">anasa</p>
+        <p id="home">Home</p>
+        <p id="explore">Explore</p>
+        <p id="about">About</p>
+        <p id="contact">Contact</p>
+        <div id="cart">
+          <p id="cart-number">1</p>
+          <img src={cart} alt="cart icon"/>
+        </div>
+        <img id="search" src={search} alt="search icon"/>
+      </nav>
+
+      {/* hero image slider */}
+      <section id="hero-img">
+          <div id="hero-text">
+            The best collection of beautiful African Art
+          </div>
+      </section>
+
+      {/* this weeks top picks */}
+      <section id="top-picks">
+        <h1 id="top-picks-heading">Top picks of the week</h1>
+        <div id="top-picks-images">
+           <img alt="weekly pick"/>
+           <img alt="weekly pick"/>
+           <img alt="weekly pick"/>
+           <img alt="weekly pick"/>
+           <img alt="weekly pick"/>
+        </div>
+
+      </section>
     </div>
   );
 }

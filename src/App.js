dc3da2e8eb2f65edba33326dec6b14725b37c2b5
gachez <briangach98@gmail.com>
2019-12-27 16:50:51 +0300

    image thumbnails

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css'
 import cart from './images/shopping-cart.png'
 import search from './images/magnifying-glass.png'
@@ -6,109 +6,143 @@ import firstpaint from './images/first.jpg'
 import afroart from './images/afroart.jpg'
 import afro from './images/afro.jpg'
 import ibrahim from './images/ibrahim-al-salahi.png'
+import stars from './images/star.png'
 
 
+export default class App extends Component {
 
-function App() {
-  return (
-    <div className="Homepage">
+  state={
+    isLoaded: false
+  }
 
-      {/* menu */}
-      <nav className="menu">
-        <p id="logo">anasa</p>
-        <p id="home">Home</p>
-        <p id="explore">Explore</p>
-        <p id="about">About</p>
-        <p id="contact">Contact</p>
-        <div id="cart">
-          <p id="cart-number">1</p>
-          <img src={cart} alt="cart icon"/>
-        </div>
-        <img id="search" src={search} alt="search icon"/>
-      </nav>
+  componentDidMount(){
+    this.setState({
+      isLoaded: true
+    })
+  }
+  render(){
 
-      {/* hero image slider */}
-      <section id="hero-img">
-          <div id="hero-text">
-            The best collection of beautiful African Art
-          </div>
-      </section>
+    if(this.state.isLoaded){
 
-      {/* this weeks top picks */}
-      <section id="top-picks">
-        <h1 id="top-picks-heading">Top picks of the week</h1>
-        <br />
-        <div id="top-picks-images">
-           <div style={{backgroundImage: `url(${ibrahim})`}}><button>Shop Now</button></div>
-           <div style={{backgroundImage: `url(${afro})`}}><button>Shop Now</button></div>
-           <div style={{backgroundImage: `url(${afroart})`}}><button>Shop Now</button></div>
-          
-          
-        </div>
 
-      </section>
-      <br />
-
-      {/* paintings section */}
-      <section id="paintings">
-        <h1 id="paintings-heading">Paintings</h1>
-        <br />
-        <div id="paintings-thumbnail">
-           <div style={{backgroundImage: `url(${ibrahim})`}}>
-              <div className="overlay"></div>
-           </div>
-           <div style={{backgroundImage: `url(${afro})`}}>
-              <div className="overlay"></div>
-           </div>
-           <div style={{backgroundImage: `url(${afroart})`}}>
-              <div className="overlay"></div>
-           </div>
-           <div style={{backgroundImage: `url(${ibrahim})`}}>
-              <div className="overlay"></div>
-           </div>
-           <div style={{backgroundImage: `url(${afro})`}}>
-              <div className="overlay"></div>
-           </div>
-           <div style={{backgroundImage: `url(${afroart})`}}>
-           <div className="overlay"></div>
-           </div>
-        </div>
-      </section>
-      <br />
-
-            {/* sculptures section */}
-      <section id="sculptures">
-        <h1 id="sculptures-heading">Sculptures</h1>
-        <br />
-        <div id="sculptures-thumbnail">
-           <div style={{backgroundImage: `url(${ibrahim})`}}>
-              <div className="overlay-1"></div>
-           </div>
-           <div style={{backgroundImage: `url(${afro})`}}>
-              <div className="overlay-1"></div>
-           </div>
-           <div style={{backgroundImage: `url(${afroart})`}}>
-              <div className="overlay-1"></div>
-           </div>
-           <div style={{backgroundImage: `url(${ibrahim})`}}>
-              <div className="overlay-1"></div>
-           </div>
-           <div style={{backgroundImage: `url(${afro})`}}>
-              <div className="overlay-1"></div>
-           </div>
-           <div style={{backgroundImage: `url(${afroart})`}}>
-           <div className="overlay-1"></div>
-           </div>
-        </div>
-      </section>
-      <br />
-
-      <footer>
-        
-      </footer>
+      return (
+        <div className="Homepage" id="home-page">
     
-    </div>
-  );
+          {/* menu */}
+          <nav className="menu" id="menu">
+            <p id="logo">anasa</p>
+            <p id="home">Home</p>
+            <p id="explore">Explore</p>
+            <p id="about">About</p>
+            <p id="contact">Contact</p>
+            <div id="cart">
+              <p id="cart-number">1</p>
+              <img src={cart} alt="cart icon"/>
+            </div>
+            <img id="search" src={search} alt="search icon"/>
+          </nav>
+    
+          {/* hero image slider */}
+          <section id="hero-img">
+              <div id="hero-text">
+                The best collection of beautiful African Art
+              </div>
+          </section>
+    
+          {/* this weeks top picks */}
+          <section id="top-picks">
+            <h1 id="top-picks-heading">Top picks of the week</h1>
+            <br />
+            <div id="top-picks-images">
+               <div style={{backgroundImage: `url(${ibrahim})`}}><button>Shop Now</button></div>
+               <div style={{backgroundImage: `url(${afro})`}}><button>Shop Now</button></div>
+               <div style={{backgroundImage: `url(${afroart})`}}><button>Shop Now</button></div>
+              
+              
+            </div>
+    
+          </section>
+          <br />
+    
+          {/* paintings section */}
+          <section id="paintings">
+            <h1 id="paintings-heading">Paintings</h1>
+            <br />
+            <div id="paintings-thumbnail">
+               <div className="overlay-div" style={{backgroundImage: `url(${ibrahim})`}}>
+                  <div className="overlay">
+                    <div className="overlay-stars">
+                      <img src={stars}/>
+                      <img src={stars}/>
+                      <img src={stars}/>
+                      <img src={stars}/>
+                      <img src={stars}/>
+                    </div>
+                    <p id="price-overlay">$900</p>
+                  </div>
+               </div>
+               <div className="overlay-div" style={{backgroundImage: `url(${afro})`}}>
+                  <div className="overlay"></div>
+               </div>
+               <div className="overlay-div" style={{backgroundImage: `url(${afroart})`}}>
+                  <div className="overlay"></div>
+               </div>
+               <div className="overlay-div" style={{backgroundImage: `url(${ibrahim})`}}>
+                  <div className="overlay"></div>
+               </div>
+               <div className="overlay-div" style={{backgroundImage: `url(${afro})`}}>
+                  <div className="overlay"></div>
+               </div>
+               <div className="overlay-div" style={{backgroundImage: `url(${afroart})`}}>
+               <div className="overlay"></div>
+               </div>
+            </div>
+          </section>
+          <br />
+    
+                {/* sculptures section */}
+          <section id="sculptures">
+            <h1 id="sculptures-heading">Sculptures</h1>
+            <br />
+            <div id="sculptures-thumbnail">
+               <div style={{backgroundImage: `url(${ibrahim})`}}>
+                  <div className="overlay-1"></div>
+               </div>
+               <div style={{backgroundImage: `url(${afro})`}}>
+                  <div className="overlay-1"></div>
+               </div>
+               <div style={{backgroundImage: `url(${afroart})`}}>
+                  <div className="overlay-1"></div>
+               </div>
+               <div style={{backgroundImage: `url(${ibrahim})`}}>
+                  <div className="overlay-1"></div>
+               </div>
+               <div style={{backgroundImage: `url(${afro})`}}>
+                  <div className="overlay-1"></div>
+               </div>
+               <div style={{backgroundImage: `url(${afroart})`}}>
+               <div className="overlay-1"></div>
+               </div>
+            </div>
+          </section>
+          <br />
+    
+          <footer>
+    
+          </footer>
+        
+        </div>
+      );
+
+     
+    
+
+    }
+
+    return null
+
+   
+  }
 }
 
-export default App;
+

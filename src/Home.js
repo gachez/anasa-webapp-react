d182a2bc671751f29ce22818ea317633f6ffcc0d
gachez <briangach98@gmail.com>
2020-01-02 17:32:09 +0300

    additional sections

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import {Nav,NavDropdown,Form,FormControl,Button} from 'react-bootstrap'
 import Carousel from 'react-bootstrap/Carousel'
+import {Card, CardDeck,Col} from 'react-bootstrap'
 import './styles-css/Home.css'
 import cart from './images/shopping-cart.png'
 import hero from './images/hero-image.jpg'
 import humble from './images/humble.jpg'
 import sculpture from './images/woman-african-people-black-girl-female.jpg'
+import first from './images/first.jpg'
+import afroart from './images/afroart.jpg'
+import ibrahim from './images/ibrahim-al-salahi.png'
+import woman from './images/womanology.jpg'
+import paint from './images/2971415-PIZGYPIE-7.jpg'
+import afro from './images/afro.jpg'
+import download from './images/download.jpeg'
+import village from './images/interesting-village-cultural-african-wallpaper-preview.jpg'
+import wooden from './images/wooden-mask-africa-carved-figure.jpg'
+import modern from './images/Modern_Makonde_carving_in_ebony.jpg'
+
 
 export default class Home extends Component {
 
@@ -102,6 +114,106 @@ export default class Home extends Component {
           </Carousel>
           
           {/* HERO SECTION ENDS HERE */}
+          <br /><br />
+          {/* TOP PICKS SECTION STARTS HERE */}
+          <h1 style={{padding : '55px'}}>Top picks of the week</h1>
+          <CardDeck>
+            <Card className="card">
+              <Card.Img variant="top" src={first} />
+              <Button variant="light" id="first-btn" className="shop-btn">Shop Now</Button>
+            
+ 
+            </Card>
+            <Card className="card">
+              <Card.Img variant="top" src={afroart} />
+              <Button variant="light" className="shop-btn">Shop Now</Button>
+ 
+            </Card>
+            <Card className="card">
+              <Card.Img variant="top" src={ibrahim} />
+              <Button variant="light" className="shop-btn">Shop Now</Button>
+             
+            
+            </Card>
+          </CardDeck>
+
+          {/* TOP PICKS SECTION ENDS HERE */}
+          <br /><br/>
+          {/* PAINTINGS SECTION STARTS HERE */}
+          <h1 style={{padding: '55px'}}>Paintings</h1>
+            <CardDeck>
+            <Col sm={4}>
+            <Card className="card">
+              <Card.Img variant="top" src={afro} />
+              <Button variant="dark" className="shop-btn">Shop Now</Button>
+            
+ 
+            </Card>
+            </Col>  
+           
+           <Col sm={4}>
+           <Card className="card" >
+              <Card.Img variant="top" src={woman}/>
+              <Button variant="dark" className="shop-btn">Shop Now</Button>
+ 
+            </Card>
+
+           </Col>
+           <Col>
+           <Card className="card" >
+              <Card.Img variant="top" src={download} />
+              <Button variant="dark"  className="shop-btn">Shop Now</Button>
+            
+ 
+            </Card>
+           </Col>         
+           
+           <Col sm={4}>
+           <Card className="card">
+              <Card.Img variant="top" src={woman} />
+              <Button variant="dark" className="shop-btn">Shop Now</Button>
+ 
+            </Card>
+           </Col>
+           
+          </CardDeck>
+            
+          
+          {/* PAINTINGS SECTION ENDS HERE */}
+          <br /><br />
+          
+          {/* SCULPTURES SECTION STARTS HERE */}
+             <h1 style={{padding: '55px'}}>Sculptures</h1>
+            <CardDeck>
+            <Col sm={4}>
+            <Card className="card">
+              <Card.Img variant="top" src={village} />
+              <Button variant="dark"  className="shop-btn sculpt">Shop Now</Button>
+            
+ 
+            </Card>
+            </Col>  
+           
+           <Col sm={4}>
+           <Card className="card" sm={4}>
+              <Card.Img variant="top" src={modern} />
+              <Button variant="dark" className="shop-btn sculpt">Shop Now</Button>
+ 
+            </Card>
+
+           </Col>
+           <Col sm={4}>
+           <Card className="card" >
+              <Card.Img variant="top" src={wooden}/>
+              <Button variant="dark" className="shop-btn sculpt">Shop Now</Button>
+             
+            
+            </Card>
+           </Col>
+                     
+         
+          </CardDeck>
+          {/* SCULPTURES SECTION ENDS HERE */}
 
         </div>
       );
